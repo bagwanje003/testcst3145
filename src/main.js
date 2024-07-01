fdd56c527b46d75cf1c3614343a55718202f3436
bagwanje003 <165358717+bagwanje003@users.noreply.github.com>
2024-07-01 10:39:27 +0400

    fixes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import '../public/styles.css'
+import '../public/css/bootstrap.css'
+import "../public/styles.css"
+import "../public/css/bootstrap.css"
+import "../public/css/bootstrap.min.css"
+ 
 
 
 
